5072bb216ef8e2738e8a396fccd8f2c07f26ca98
ivan-teotonio <itaj@discente.ifpe.edu.br>
2024-11-20 14:50:50 -0300

    Revisado

--- conteudo-pdf.php
+++ conteudo-pdf.php
@@ -1,10 +1,10 @@
 <?php
-require "src/conexao-bd.php";
+require "src/conexao.php";
 require "src/Modelo/Produto.php";
 require "src/Repositorio/ProdutoRepositorio.php";
 
 $produtoRepositorio = new ProdutoRepositorio($pdo);
-$produtos = $produtoRepositorio->buscarTodos();
+$produtos = $produtoRepositorio->buscarTodosProdutos();
 ?>
 
 <style>
